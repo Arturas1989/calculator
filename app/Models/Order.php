23352d1dc239c9d7da35741a01b834f00d366bb5
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2021-07-17 18:03:12 +0300

    simplified method

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -28,19 +28,12 @@ class Order extends Model
 
     public function notCreatedProducts()
     {
-        $orders = Order::where('product_id','=',null)->get()->all();
-        $orderArr = [];
-        foreach ($orders as $order) {
-            $product = Product::where('code','=',$order->code)->get()->first();
-            if($product!=null){
-                $order->product_id = $product->id;
-                $order->save();
-            }
-            else{
-                $orderArr[] = $order; 
-            }
-        }
-        return $orderArr; 
+        return  Order::select('code')->distinct()->where('product_id','=',null)->get()->all();
+    }
+
+    public function createdProducts()
+    {
+        return Order::where('product_id','!=',null)->get()->all();
     }
     
     public function errorsHTML($field, $errors)
